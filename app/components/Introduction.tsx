2fd44fd8183bbcee1f70281958d646155da9b165
rohittttt29 <131178081+Rohit29-hub@users.noreply.github.com>
2024-02-27 16:38:49 +0530

    Update Introduction.tsx

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -11,7 +11,7 @@ const Introduction = () => {
             <div className='relative w-full h-auto'>
                 <div className='lg:absolute left-3 w-auto p-1 h-auto mt-6 sm:mt-0 static'>
                     <h1 className='sm:text-[6rem] lg:text-[9rem] md:text-[7.5rem] text-[4rem] font-bold text-[#000000d8]' id='greet-1'>Hii all,</h1>
-                    <h1 className='sm:text-[6rem] lg:text-[9rem] md:text-[7.5rem] text-[3.5rem] sm:mt-[-3rem] font-bold text-[#000000d8]' id='greet-2'>I am Rohit <span id='hii-img' className='sm:text-[4rem] md:text-[5rem] text-[3.5rem]'>👋</span></h1>
+                    <h1 className='sm:text-[6rem] lg:text-[9rem] md:text-[7.5rem] text-[3.5rem] sm:mt-[-3rem] font-bold text-[#000000d8]' id='greet-2'>I am Rohit <span id='hii-img' className='sm:text-[4rem] md:text-[5rem] text-[2.5rem]'>👋</span></h1>
                     <div className='sm:w-96 md:w-[35rem] w-full h-auto mt-5'>
                         <p id='description-about-me' className='text-1xl sm:text-[19px] text-[#000000d8]'>
                             A passionate <span className='font-bold '>Full Stack Web Developer(MERN)</span>. having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Nextjs / Nodejs(Expressjs), Mongodb and some other cool libraries and frameworks.
